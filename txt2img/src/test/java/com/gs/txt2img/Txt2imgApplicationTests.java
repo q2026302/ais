09a64305cb4b661aa915f4c0c0e8d7075788226a
沫沫 <momo-qqbot522@users.noreply.github.com>
2026-07-08 16:31:32 +0800

    init commit

---
+++ txt2img/src/test/java/com/gs/txt2img/Txt2imgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gs.txt2img;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Txt2imgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
